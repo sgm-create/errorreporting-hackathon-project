bf7dee37d81032f5154454f8530ab830751ec2c5
sgm-create <sayanighoshmahapatra@gmail.com>
2025-08-25 16:55:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -854,6 +854,4 @@ def health_check():
         return jsonify({'status': 'error', 'error': str(e)})
 
 if __name__ == '__main__':
-    conn = sqlite3.connect("environment.db")
-    conn.close()
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
